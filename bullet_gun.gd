595af3f3e0b641e3cfd07c49f4836489b0f1d48a
Joy Fulness <fulness4joy@gmail.com>
2024-09-06 11:58:26 +0300

    Added portal and sparks from bullets

--- bullet_gun.gd
+++ bullet_gun.gd
@@ -4,11 +4,10 @@ const speed = 50
 const damage_value = 1
 
 var time_disapear = 5
-
+@onready var sparks_scene = load("res://sparks.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	apply_central_impulse(transform.basis * Vector3(0, 0, -speed))
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -16,9 +15,16 @@ func _physics_process(delta):
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
+		var sparks = sparks_scene.instantiate()
+		sparks.position = global_position
+		get_tree().get_root().add_child(sparks)
+		
+		sparks.connect("finished", sparks.queue_free)
+		
 		if body.is_in_group("enemy"):
 			body.damage(damage_value)
-			queue_free()
+			
+		queue_free()	
 			
 	time_disapear -= delta
 	
